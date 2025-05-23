d6a7bfe0934a1af74ac9b8773124b4bb1e1d70d5
codx333 <monneyphilipp@gmail.com>
2025-05-23 23:11:27 +0100

    Fix SWC binary loading issue

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    swcMinify: true,
+    forceSwcTransforms: true,
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
